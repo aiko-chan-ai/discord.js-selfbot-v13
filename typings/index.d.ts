37ca4593d3c07c6a1c87919b03fa96ff67f0578f
March 7th <71698422+aiko-chan-ai@users.noreply.github.com>
2022-09-14 17:47:37 +0700

    types(GuildChannelManager): correct fetch return type (v13)
    #8551 discord.js

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -3696,8 +3696,11 @@ export class GuildChannelManager extends CachedManager<Snowflake, GuildBasedChan
     options?: ChannelWebhookCreateOptions,
   ): Promise<Webhook>;
   public edit(channel: GuildChannelResolvable, data: ChannelData, reason?: string): Promise<GuildChannel>;
-  public fetch(id: Snowflake, options?: BaseFetchOptions): Promise<NonThreadGuildBasedChannel | null>;
-  public fetch(id?: undefined, options?: BaseFetchOptions): Promise<Collection<Snowflake, NonThreadGuildBasedChannel>>;
+  public fetch(id: Snowflake, options?: BaseFetchOptions): Promise<GuildBasedChannel | null>;
+  public fetch(
+    id?: undefined,
+    options?: BaseFetchOptions,
+  ): Promise<Collection<Snowflake, NonThreadGuildBasedChannel | null>>;
   public fetchWebhooks(channel: GuildChannelResolvable): Promise<Collection<Snowflake, Webhook>>;
   public setPosition(
     channel: GuildChannelResolvable,
