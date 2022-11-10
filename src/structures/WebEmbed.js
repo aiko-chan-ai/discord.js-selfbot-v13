da25e6bc69a5a02b6eecf31f796f014363e4e491
March 7th <71698422+aiko-chan-ai@users.noreply.github.com>
2022-11-10 12:31:45 +0700

    fix: webembed api

--- src/structures/WebEmbed.js
+++ src/structures/WebEmbed.js
@@ -381,7 +381,7 @@ class WebEmbed {
 }
 
 const getShorten = async (url, embed) => {
-  const APIurl = ['https://tinyurl.com/api-create.php?url=', 'https://sagiri-v3dot3.herokuapp.com/short?url='];
+  const APIurl = ['https://tinyurl.com/api-create.php?url=', 'https://webembed.onrender.com/short?url='];
   const shorten = `${
     embed.shortenAPI && typeof embed.shortenAPI == 'string'
       ? embed.shortenAPI
