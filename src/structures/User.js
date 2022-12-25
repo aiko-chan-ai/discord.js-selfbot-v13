609c9acc789a412a874feb349419030b0956aa56
March 7th <71698422+aiko-chan-ai@users.noreply.github.com>
2022-12-25 18:17:42 +0700

    fix(User): get VoiceState from guild cache

--- src/structures/User.js
+++ src/structures/User.js
@@ -229,7 +229,11 @@ class User extends Base {
    * @readonly
    */
   get voice() {
-    return this.client.voiceStates.cache.get(this.id) ?? new VoiceState({ client: this.client }, { user_id: this.id });
+    return (
+      this.client.voiceStates.cache.get(this.id) ??
+      this.client.guilds.cache.find(g => g?.voiceStates?.cache?.get(this.id))?.voiceStates?.cache?.get(this.id) ??
+      new VoiceState({ client: this.client }, { user_id: this.id })
+    );
   }
 
   _ProfilePatch(data) {
