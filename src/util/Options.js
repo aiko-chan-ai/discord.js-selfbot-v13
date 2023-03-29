ca50d03688b271bc4bd86232bfdc5f744d4e7684
Elysia <71698422+aiko-chan-ai@users.noreply.github.com>
2023-03-29 19:20:32 +0700

    chore: release v2.12.2
    - fetch members

--- src/util/Options.js
+++ src/util/Options.js
@@ -201,7 +201,7 @@ class Options extends null {
           os_version: '10.0.22621',
           os_arch: 'x64',
           system_locale: 'en-US',
-          client_build_number: 183813,
+          client_build_number: 184344,
           native_build_number: 30921,
           client_event_source: null,
           design_id: 0,
