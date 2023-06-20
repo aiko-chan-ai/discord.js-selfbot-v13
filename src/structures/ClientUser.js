ba98c1bdaae6ce902432fc920614b48b87fc8340
Mega <fknmega@protonmail.com>
2023-06-20 16:52:54 +0300

    Update ClientUser.js

--- src/structures/ClientUser.js
+++ src/structures/ClientUser.js
@@ -617,7 +617,7 @@ class ClientUser extends User {
 
   /**
    * Set pronouns
-   * @param {string | null} of pronouns
+   * @param {string | null} pronouns
    * @returns {Promise<ClientUser>}
    */
   setPronouns(pronouns = '') {
