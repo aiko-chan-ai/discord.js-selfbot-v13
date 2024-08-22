a168858aaf9a06ef4304aa29d53e372515e962f1
Sans <002notsans@gmail.com>
2024-08-22 22:17:36 +0200

    Update ChannelManager.js

--- src/managers/ChannelManager.js
+++ src/managers/ChannelManager.js
@@ -128,7 +128,7 @@ class ChannelManager extends CachedManager {
     recipients = recipients
       .map(r => this.client.users.resolveId(r))
       .filter(r => r && this.client.relationships.cache.get(r) == RelationshipTypes.FRIEND);
-    if (recipients.length < 1 || recipients.length > 9) throw new Error('Invalid Users length (max=9)');
+    if (recipients.length == 1 || recipients.length > 9) throw new Error('Invalid Users length (max=9)');
     const data = await this.client.api.users['@me'].channels.post({
       data: { recipients },
     });
