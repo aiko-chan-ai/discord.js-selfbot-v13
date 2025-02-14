e53ff466cd8bc5b35dc98b29994ac5a7546a4943
Elysia <71698422+aiko-chan-ai@users.noreply.github.com>
2025-02-14 22:16:53 +0700

    fix(Guild): type error with permissionOverwrites
    #10527 djs

--- src/structures/PermissionOverwrites.js
+++ src/structures/PermissionOverwrites.js
@@ -181,7 +181,9 @@ class PermissionOverwrites extends Base {
     }
 
     const userOrRole = guild.roles.resolve(overwrite.id) ?? guild.client.users.resolve(overwrite.id);
-    if (!userOrRole) throw new TypeError('INVALID_TYPE', 'parameter', 'User nor a Role');
+    if (!userOrRole) {
+      throw new TypeError('INVALID_TYPE', 'parameter', 'cached User or Role');
+    }
     const type = userOrRole instanceof Role ? OverwriteTypes.role : OverwriteTypes.member;
 
     return {
