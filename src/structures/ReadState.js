4caccc3330dfe717dd3080b778fc334a5c568e24
jsoncitron <194698371+jsoncitron@users.noreply.github.com>
2025-03-05 18:52:56 +0200

    Update ReadState.js

--- src/structures/ReadState.js
+++ src/structures/ReadState.js
@@ -9,21 +9,30 @@ const Base = require('./Base');
 class ReadState extends Base {
   constructor(client, data) {
     super(client);
+    this._patch(data);
+  }
+
+  _patch(data) {
     /**
-     * The channel id of the read state
+     * The resource id of the read state
      * @type {Snowflake}
      */
     this.id = data.id;
     /**
+     * The type of the read state
+     * @type {?ReadStateType}
+     */
+    this.type = ReadStateTypes[data.read_state_type ?? 0];
+    /**
      * The mention count of the resource
      * @type {number}
      */
-    this.mentionCount = data.mention_count;
+    this.mentionCount = data.mention_count ?? data.badge_count ?? 0;
     /**
-     * Day when the resource was last viewed
+     * Days since 2015 when the resource was last viewed
      * @type {number}
      */
-    this.lastViewed = data.last_viewed + 16436000;
+    this.lastViewed = data.last_viewed;
     if (data.last_pin_timestamp) {
       let lastPinTimestamp = Date.parse(data.last_pin_timestamp);
       /**
@@ -40,6 +49,26 @@ class ReadState extends Base {
      */
     this.lastMessageId = data.last_acked_id ?? data.last_message_id ?? null;
     // TODO Readstateflags
+  }
+
+  /**
+   * The resource of the read state
+   * @type {TextBasedChannel | Guild | ClientUser}
+   * @readonly
+   */
+  get resource() {
+    if (this.type === 'CHANNEL') return this.client.channels.get(this.id);
+    if (['GUILD_SCHEDULED_EVENT', 'GUILD_HOME', 'GUILD_ONBOARDING_QUESTION'].includes(this.type)) return this.client.guilds.get(this.id);
+    return this.client.user;
+  }
+
+  /**
+   * Deletes the read state. Only applicable to 'CHANNEL' read states
+   *
+   * @returns {Promise<>}
+   */
+  delete() {
+    return this.client.api.channels[this.id].messages.ack.delete();
   }
 }
 
