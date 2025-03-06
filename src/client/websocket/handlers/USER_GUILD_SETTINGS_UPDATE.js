e56c1ad3ad4fde7086f919776063951c5cf0d9f7
jsoncitron <194698371+jsoncitron@users.noreply.github.com>
2025-03-06 23:34:43 +0200

    Update USER_GUILD_SETTINGS_UPDATE.js

--- src/client/websocket/handlers/USER_GUILD_SETTINGS_UPDATE.js
+++ src/client/websocket/handlers/USER_GUILD_SETTINGS_UPDATE.js
@@ -1,6 +1,26 @@
 'use strict';
 
+const { Events } = require('../../../util/Constants');
+
 module.exports = (client, { d: data }) => {
-  const guild = client.guilds.cache.get(data.guild_id);
-  if (guild) guild?.settings._patch(data);
+  if (data.guild_id === null) {
+    const before = client.guildSettings._copy();
+    client.guildSettings._patch(data);
+  
+    /**
+     * Emitted when guild settings are updated.
+     * @event Client#guildSettingsUpdate
+     * @param {GuildSettingManager} before The guild setting
+     * @param {GuildSettingManager} after The guild setting
+     */
+    client.emit(Events.USER_GUILD_SETTINGS_UPDATE, before, client.guildSettings);
+  } else {
+    let guild = client.guilds.cache.get(data.guild_id);
+    if (!guild) return;
+    
+    const before = client.guildSettings._copy();
+    guild.settings._patch(data);
+
+    client.emit(Events.USER_GUILD_SETTINGS_UPDATE, before, guild.settings);
+  }
 };
